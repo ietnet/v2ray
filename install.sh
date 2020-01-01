2ddf1ee724156cd426212bf832de32ec04d35776
ietnet <59407326+ietnet@users.noreply.github.com>
2020-01-01 16:16:18 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1093,11 +1093,11 @@ esac
 clear
 while :; do
 	echo
-	echo "........... V2Ray 一键安装脚本 & 管理脚本 by 233v2.com .........."
+	echo "........... V2Ray 一键安装脚本 & 管理脚本 by ietnet.com .........."
 	echo
-	echo "帮助说明: https://233v2.com/post/1/"
+	echo "帮助说明: https://ietnet.com/post/1/"
 	echo
-	echo "搭建教程: https://233v2.com/post/2/"
+	echo "搭建教程: https://ietnet.com/post/2/"
 	echo
 	echo " 1. 安装"
 	echo
